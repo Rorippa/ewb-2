3f6a4d5c4614f1751b65d183ec5d8401066e4b84
Rorippa <koivutar@gmail.com>
2014-08-28 00:04:38 +0300

    no message

---
+++ charter.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<!--[if lt IE 9]>
+<script src="js/html5shiv.min.js"></script>
+<![endif]-->
+
+<title>Insinöörit ilman rajoja</title>
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" media="screen and (min-width: 1025px)" href="css/style.css" />
+<link rel="stylesheet" media="screen and (min-width: 990px) and (max-width: 1100px)" href="css/tablet-landscape.css" />
+<link rel="stylesheet" media="screen and (min-width: 800px) and (max-width: 989px)" href="css/tablet-portrait.css" />
+<link rel="stylesheet" media="screen and (min-width: 640px) and (max-width: 799px)" href="css/smartphone-landscape.css" />
+<link rel="stylesheet" media="screen and (max-width: 639px)" href="css/smartphone-portrait.css" />
+
+
+</head>
+
+<body>
+	
+	<header>
+		<a href="home.html"><img src="insiniors.png" id="logo" /></a>
+		<a href="home.html"><img src="lamppu.jpg" id="logo-tablet" /></a>
+	</header>
+	
+	<nav role="navigation" class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <!-- Collection of nav links, forms, and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+					<a data-toggle="dropdown" class="dropdown-toggle" href="#">Organisation<b class="caret"></b></a>
+				    <ul role="menu" class="dropdown-menu">
+						<li><a href="#">Charter</a></li>
+                        <li><a href="#">Board</a></li>
+                        <li><a href="#">Sponsors</a></li>
+                        <li><a href="#">Documents</a></li>
+                    </ul>
+				</li>
+                <li class="dropdown">
+					<a data-toggle="dropdown" class="dropdown-toggle" href="#">Projects<b class="caret"></b></a>
+					<ul role="menu" class="dropdown-menu">
+                        <li><a href="#">Past Projects</a></li>
+					</ul>
+				</li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+				<li>
+                    <a href="#">Events</a>
+                </li>
+				<li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Get involved<b class="caret"></b></a>
+					<ul role="menu" class="dropdown-menu">
+						<li><a href="#">Join us</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>
+                </li>
+				<li>
+                    <a href="#">Old site</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+	<div id="sponsor-mobile">
+
+	<!-- Mobiilisponsorit -->
+	
+	</div>
+	<div id="sponsor">
+	
+	
+	<!-- Sponsorit tähän -->
+
+	
+	</div>	
+	
+	<div id="main" class="site-main">
+		
+		<div id="content" class="site-content" role="main"> 
+		<ol>
+		<li>Yhdistyksen nimi ja kotipaikka</li>
+		<p>Yhdistyksen nimi on Insinöörit ilman rajoja ry ja sen kotipaikka on Espoo.</p>
+		<li>Tarkoitus ja toiminnan laatu</li>
+		<p>Yhdistys on poliittisesti ja uskonnollisesti sitoutumaton, voittoa tuottamaton yhdistys, jonka tarkoituksen on edistää inhimillistä kehitystä soveltuvan ja kestävän teknologian ja insinööritaidon avulla sekä lisätä jäsentensä ja yleisön tietoisuutta tärkeistä kehityskysymyksistä kuten koulutuksesta, terveydestä, ravinnosta, asumisesta, tiedonvälityksestä ja ympäristöstä sekä teknologian mahdollisuuksista niiden ratkaisemisessa.
+		</p>
+		<p>
+		Tarkoituksensa toteuttamiseksi yhdistys
+		<ul>
+		<li>kouluttaa jäsenkuntaansa esim. kehitykseen ja teknologiaan liittyvissä asioissa</li>
+		<li>järjestää tiedotus- ja keskustelutilaisuuksia</li>
+		<li>tiedottaa ajankohtaisista ja tärkeistä aiheista eri kanavilla</li>
+		<li>järjestää tapaamisia jäsentensä sekä tekniikan ja kehityksen asiantuntijoiden kesken</li>
+		<li>toteuttaa projekteja yhdessä muiden tahojen kanssa Suomessa ja muualla maailmassa</li>
+		<li>tekee tutkimustyötä kehityskysymysten ja teknisten ratkaisujen alueella</li>
+		</ul>
+		</p>
+		<p>
+		Toimintansa tukemiseksi yhdistys
+		<ul>
+		<li>hakee apurahoja ja stipendejä</li>
+		<li>perii jäseniltään jäsenmaksua</li>
+		<li>ottaa vastaan lahjoituksia</li>
+		</ul>
+		</p>
+		<li>Jäsenet</li>
+		<p>Yhdistyksen varsinaiseksi jäseneksi voidaan hyväksyä insinööri tai muukin henkilö, joka hyväksyy yhdistyksen tarkoituksen.</p>
+
+		<p>Kannatusjäseneksi voidaan hyväksyä yksityinen henkilö tai oikeuskelpoinen yhteisö, joka haluaa tukea yhdistyksen tarkoitusta ja toimintaa.</p>
+
+		<p>Varsinaiset jäsenet ja kannattavat jäsenet hyväksyy hakemuksesta yhdistyksen hallitus.</p>
+
+		<p>Kunniapuheenjohtajaksi tai kunniajäseneksi voidaan hallituksen esityksestä yhdistyksen kokouksessa kutsua henkilö, joka on huomattavasti edistänyt ja tukenut yhdistyksen toimintaa.</p>
+		
+		<li>Jäsenen eroaminen ja erottaminen</li>
+		<p>Jäsenellä on oikeus erota yhdistyksestä ilmoittamalla siitä kirjallisesti hallitukselle tai sen puheenjohtajalle tai ilmoittamalla erosta yhdistyksen kokouksessa merkittäväksi pöytäkirjaan.</p>
+		</ol>
+		
+		<!-- Sisältö tähän -->
+		
+		
+		</div><!-- #content .site-content -->
+		
+		<div id="secondary">
+ <?php include 'uutiset.txt'; ?> 
+		<!-- Uutiset tähän -->
+
+		
+		</div><!-- #secondary -->
+	
+	</div><!-- #main .site-main -->
+	
+	<footer role="contentinfo">
+	</footer>
+
+</body>
+
+</html>
